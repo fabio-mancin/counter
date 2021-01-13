6f5b05daee3ed275b1ae63dc5939df6d2ba2e8e5
fender1990 <fabio.mancin90@gmail.com>
2021-01-14 00:47:38 +0100

    New features
    Added reset button, commented code, revamped scrièts

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,58 @@
 document.addEventListener("DOMContentLoaded", function () {
+    //selectors of elements for clarity
     const buttonsNodeList = document.querySelectorAll(".button");
     const counterSelector = document.querySelector(".counter");
     const inputSelector = document.querySelector(".input");
-  
-    buttonsNodeList.forEach(button => {
-        const modifyBy = () => {
-            const modifier = Number(inputSelector.value);
-            return modifier
-        }
+    const resetSelector = document.querySelector(".reset");
+    const labelSelector = document.querySelector(".label");
 
-        button.addEventListener("click", (e)=>{
-            const hasPlusClass = e.target.classList.contains("plus");
-            let counterNumber = Number(counterSelector.innerHTML);
-            hasPlusClass ? 
-                counterSelector.innerHTML=counterNumber+modifyBy() : 
-                counterSelector.innerHTML=counterNumber-modifyBy()
+    //show a message instead of the default text..
+    const showMessage = (message) => {
+        labelSelector.innerHTML = message;
+        //..and go back to default after 3 seconds
+        setTimeout(() => {
+            labelSelector.innerHTML = "Add/Subtract by:";
+        }, 3000);
+    };
+
+    //add event listener to every button
+    buttonsNodeList.forEach(button => {
+        //helper function to retrieve the modifier the user needs from input
+        const modifyBy = (plusOrMinus) => {
+            const counterNumber = Number(counterSelector.innerHTML);
+            const modifier = Number(inputSelector.value);
+            return plusOrMinus === "plus" ? 
+                        counterNumber+modifier : 
+                        counterNumber-modifier
+        }
+        //actually adding the event listener
+        button.addEventListener("click", (e) => {
+            //check which button was clicked
+            const plusOrMinus = e.target.classList.contains("plus") ? 
+                                    "plus" : 
+                                    "minus"
+            //then set the counter to the desired value                        
+            counterSelector.innerHTML = modifyBy(plusOrMinus);
         });
     });
 
-    inputSelector.addEventListener("keyup", ()=>{
-        const labelSelector = document.querySelector(".label");
-        const hideWarning = setTimeout(() => {
-            labelSelector.innerHTML = "Add/Subtract by:";
-        }, 3000);
+    //handle the reset button
+    resetSelector.addEventListener("click", () => {
+        counterSelector.innerHTML = 0;
+        showMessage("Counter reset.")
+    })
 
+    //check if the user is inserting valid values
+    inputSelector.addEventListener("keyup", () => {
         let currentInputValue = Number(inputSelector.value);
+        //if the value is invalid..
         if (isNaN(currentInputValue)) {
-            labelSelector.innerHTML = "Only numbers or '.' allowed.";
-            inputSelector.value = inputSelector.value.slice(0,-1);
+            //..show a warning..
+            showMessage("Only numbers or '.' allowed.");
+            //..and delete the invalid value
+            inputSelector.value = inputSelector.value.slice(0, -1);
         }
-
-        inputSelector.style.width = inputSelector.value.length+2 + "ch";
+        //keep the width of the input field always nice
+        inputSelector.style.width = inputSelector.value.length + 2 + "ch";
     });
 });
